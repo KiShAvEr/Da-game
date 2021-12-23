645111b319ebf766c1edccf8d2c990d865c13c1e
varadiistvan <varadiista.1@gmail.com>
2021-12-23 18:36:43 +0100

    mindzsá kész

--- app.js
+++ app.js
@@ -2,6 +2,121 @@ const express = require('express')
 const http = require('http')
 const webSocket = require("ws")
 
+
+class GameState {
+  constructor() {
+    this.board = [[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0]]
+    this.playerOneTurn = true
+    this.wins = {
+      "playerOne": 0,
+      "playerTwo": 0,
+    }
+    this.lastMove = null
+    this.winner = 0
+  }
+
+  dropBall(col, player) {
+    
+    for(let i = 5; i >= 0; i--) {
+      if(this.board[col][i] == 0) {
+        this.board[col][i] = player
+        this.lastMove = {
+          col,
+          "row": i,
+          player: player == 1? "playerOne": "playerTwo"
+        }
+        this.playerOneTurn = !this.playerOneTurn
+
+        this.winner = checkWin(this.board)
+
+        if(this.winner == 1) {
+          this.win.playerOne += 1
+        }
+        if(this.winner == 2) {
+          this.win.playerTwo += 1
+        }
+
+        return {
+          gameState: this,
+          error: null
+        }
+      }
+    }
+
+    return {
+      gameState: this,
+      error: "Column is full!"
+    }
+  }
+
+  clearTable() {
+    this.board = [[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0]]
+    this.playerOneTurn = (this.wins.playerOne+this.wins.playerTwo)%2 == 0
+  }
+
+}
+
+
+class Game {
+  constructor(p1, p2) {
+    this.playerOne = p1
+    this.playerTwo = p2
+    this.gameState = new GameState()
+  }
+}
+
+checkWin = (board) => {
+  const diagonal = checkTheDiagonal(board)          //3000000
+  const vertical = checkTheVertical(board)
+  const horizontal = checkTheHorizontal(board)
+
+  if(diagonal != 0) {
+    return diagonal
+  }
+
+  if(vertical != 0) {
+    return vertical
+  }
+
+  if(horizontal != 0) {
+    return horizontal
+  }
+
+}
+
+checkTheVertical = (board) => {
+  for(let col of board) {
+    for(let i = 0; i < 3; i++) {
+      if(col[i] == col[i+1] && col[i] == col[i+2] && col[i] == col[i+3]) return col[i]
+    }
+  }
+  return 0
+}
+
+checkTheHorizontal = () => {
+  for(let i = 0; i < 4; i++) {
+    for(let j = 0; j < 6; j++) {
+      if(board[i][j] == board[i+1][j] && board[i][j] == board[i+2][j] && board[i][j] == board[i+3][j]) return board[i][j]
+    }
+  }
+  return 0
+}
+
+checkTheDiagonal = () => {
+  for(let i = 0; i < 4; i++) {
+    for(let j = 0; j < 3; j++) {
+      if(board[i][j] == board[i+1][j+1] && board[i][j] == board[i+2][j+2] && board[i][j] == board[i+3][j+3]) return board[i][j]
+    }
+  }
+  for(let i = 3; i < 7; i++) {
+    for(let j = 0; j < 3; j++) {
+      if(board[i][j] == board[i-1][j+1] && board[i][j] == board[i-2][j+2] && board[i][j] == board[i-3][j+3]) return board[i][j]
+    }
+  }
+  return 0
+}
+
+
 const port = process.env.PORT || process.argv[2] || 3000
 const app = express()
 
@@ -21,20 +136,66 @@ server.listen(port)
 
 const wss = new webSocket.Server({server})
 
+let queue = []
+
+let games = {}
+
 wss.on("connection", (ws) => {
 
-  ws.send("fasz vagy")
+  if(queue.length == 0) {
+    queue.push(ws)
+    ws.send("waiting")
+  }
+  else {
+    const otherPlayer = queue.shift()
+    let game
+    const order = Math.floor(Math.random()*2)
+    switch(order) {
+      case 0: {
+        game = new Game(ws, otherPlayer)
+        ws.send("playerOne")
+        otherPlayer.send("playerTwo")
+        break;
+      }
+      case 1: {
+        game = new Game(otherPlayer, ws) 
+        ws.send("playerTwo")
+        otherPlayer.send("playerOne")
+        break;
+      }
+    }
+    games[otherPlayer] = game
+    games[ws] = game
+    ws.send(JSON.stringify({
+      "gameState": game.gameState,
+      "error": null
+    }))
+    otherPlayer.send(JSON.stringify({
+      "gameState": game.gameState,
+      "error": null
+    }))
+  }
 
-  ws.on("message", (msg) => {
-    console.log(JSON.parse(msg.toJSON().data.map(x => String.fromCharCode(x)).join("")).column);
-    return false
-  })
+  ws.onmessage = (ev) => {
+    const data = JSON.parse(ev.data)
+    console.log(data)
+    switch(data.actionName) {
+      case "drop": {
+        const game = games[ws]
+        const playerNum = game.playerOne == ws? 1: 2
+        const res = game.gameState.dropBall(data.action, playerNum)
+        console.log(game)
+        game.playerOne.send(JSON.stringify(res))
+        game.playerTwo.send(JSON.stringify(res))
+        break
+      }
+      case "rematch": {
+
+      }
+    }
+  }
 
   return false
 })
 
 console.log(`Server running on port ${port}`)
-
-app.put("/board", (req, res) => {
-  res.send("pénisz" + req.body.column)
-})
--- public/javascripts/setupGame.js
+++ public/javascripts/setupGame.js
@@ -1,5 +1,23 @@
 const backArrowContainer = document.getElementById("backArrowContainer")
-const table = [[],[],[],[],[],[],[]]
+let table = [[],[],[],[],[],[],[]]
+
+const ownColor = document.getElementById("yourColor")
+const opponentColor = document.getElementById("opponentColor")
+
+let ownPlayer;
+
+const drawGameState = (lastMove) => {
+    if(lastMove) {
+        const parent = document.getElementById(`c${lastMove.col}`)
+        const child = document.createElement("div")
+        child.classList.add(lastMove.player, "token")
+
+        parent.prepend(child)
+
+
+        
+    }
+}
 
 backArrowContainer.addEventListener("click", () => {
     window.open(window.location.href.replace("board", ""), "_self")
@@ -25,34 +43,92 @@ socket.onopen = (ev) => {
 }
 
 socket.onmessage = (ev) => {
-    console.log(ev.data)
+    switch(ev.data) {
+        case "waiting": {
+            alert("waiting")
+            break
+        }
+        case "playerOne": {
+            ownColor.classList.remove("playerTwo")
+            ownColor.classList.add("playerOne")
+
+            document.getElementById("turnString").innerHTML = "Your turn!"
+
+            ownPlayer = "playerOne"
+
+            opponentColor.classList.remove("playerOne")
+            opponentColor.classList.add("playerTwo")
+            break
+        }
+        case "playerTwo": {
+            opponentColor.classList.remove("playerTwo")
+            opponentColor.classList.add("playerOne")
+
+            document.getElementById("turnString").innerHTML = "Opponent's turn!"
+
+            ownColor.classList.remove("playerOne")
+            ownColor.classList.add("playerTwo")
+
+            ownPlayer = "playerTwo"
+
+            break
+        }
+        default: {
+            const currentState = JSON.parse(ev.data).gameState
+            if(JSON.parse(ev.data).error != null) alert("cringe")
+            table = currentState.board
+            document.getElementById("yourScore").innerHTML = currentState.wins[ownPlayer]
+            document.getElementById("opponentScore").innerHTML = currentState.wins[ownPlayer == "playerOne"? "playerTwo": "playerOne"]
+
+            const yourTurn = (currentState.playerOneTurn && ownPlayer == "playerOne") || (!currentState.playerOneTurn && ownPlayer == "playerTwo")
+
+            console.log(yourTurn, ownPlayer, currentState.playerOneTurn)
+
+            switch(yourTurn) {
+                case true: {
+                    document.getElementById("turnString").innerHTML = "Your turn!"
+                    for (let i = 0; i < 7; i++) {
+                        let name = "slots" + i;
+                        const currentCanvas = document.getElementById(name);
+                        currentCanvas.addEventListener("mouseenter", () => {
+                            if (table[i][0] == 0){
+                                currentCanvas.src = "./assets/slotsdark.png"
+                            }
+                        }) 
+                        currentCanvas.addEventListener("mouseleave", () => {
+                            currentCanvas.src = "./assets/slots.png"
+                        })
+                        currentCanvas.addEventListener("click", () => {
+                            if (table[i][0] == 0){
+                                console.log("ball dropped in canvas " + i)
+                                table[i].push("x")
+                                socket.send(JSON.stringify({"action": i, "actionName": "drop"}))
+                                currentCanvas.src = "./assets/slots.png"
+                            }
+                        })   
+                    } 
+                    break
+                }
+                case false: {
+                    document.getElementById("turnString").innerHTML = "Opponent's turn!"
+                    for (let i = 0; i < 7; i++) {
+                        let name = "slots" + i;
+                        const currentCanvas = document.getElementById(name);
+                        currentCanvas.replaceWith(currentCanvas.cloneNode(true))
+                    } 
+                    break
+                }
+                default: {
+                    console.error("what")
+                }
+            }
+
+            drawGameState(currentState.lastMove)
+        }
+    }
     return false
 }
 
 socket.onclose = (ev) => {
     console.log("what", ev)
 }
-
-
-for (let i = 0; i < 7; i++) {
-    let name = "slots" + i;
-    const currentCanvas = document.getElementById(name);
-    currentCanvas.addEventListener("mouseenter", () => {
-        if (table[i].length < 6){
-            currentCanvas.src = "./assets/slotsdark.png"
-        }
-    }) 
-    currentCanvas.addEventListener("mouseleave", () => {
-        currentCanvas.src = "./assets/slots.png"
-    })
-    currentCanvas.addEventListener("click", () => {
-        if (table[i].length < 6){
-            console.log("ball dropped in canvas " + i)
-            table[i].push("x")
-            socket.send(JSON.stringify({"column": i}))
-            if (table[i].length >= 6){
-                currentCanvas.src = "./assets/slots.png"
-            }
-        }
-    })   
-} 
