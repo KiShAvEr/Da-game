1f0959b376d30241efded537da01c2525ba91d68
ppvarga <v.p.patrik57@gmail.com>
2021-12-23 10:32:05 +0100

    csinátam dógokat

--- public/javascripts/setupGame.js
+++ public/javascripts/setupGame.js
@@ -1,4 +1,5 @@
 const backArrowContainer = document.getElementById("backArrowContainer")
+const table = [[],[],[],[],[],[],[]]
 
 backArrowContainer.addEventListener("click", () => {
     window.open(window.location.href.replace("board", ""), "_self")
@@ -15,3 +16,25 @@ backArrowContainer.addEventListener("mouseleave", () => {
         element.style.fill = "black"
     }
 })
+
+for (let i = 0; i < 7; i++) {
+    let name = "c" + i;
+    const currentCanvas = document.getElementById(name);
+    currentCanvas.addEventListener("mouseenter", () => {
+        if (table[i].length < 6){
+            currentCanvas.style.background = "#134653"
+        }
+    }) 
+    currentCanvas.addEventListener("mouseleave", () => {
+        currentCanvas.style.background = "#1f6f86"
+    })
+    currentCanvas.addEventListener("click", () => {
+        if (table[i].length < 6){
+            console.log("ball dropped in canvas " + i)
+            table[i].push("x")
+            if (table[i].length >= 6){
+                currentCanvas.style.background = "#1f6f86"
+            }
+        }
+    })   
+} 
